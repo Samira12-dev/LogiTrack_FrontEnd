acacbb6092f5370a2af6dd90622a90f2b0f27b3c
Samira12-dev <samiraelboussidi663@gmail.com>
2026-08-03 23:46:41 +0100

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-
+import Header from './components/layout/Header'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
     </>
   )
 }
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import { FaTruckFast } from "react-icons/fa6";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="header-container">
+
+                <h1 className="logo">
+                    <FaTruckFast className="icon" />
+                    Logi<span>Track</span>
+                </h1>
+
+                <nav>
+                    <ul className="nav-links">
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                        <li>
+                            <Link to="/login" className="login-btn">
+                                Login
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+        </header>
+    );
+}
